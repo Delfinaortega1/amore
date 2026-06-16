4f711343ddfaa1a341bf4545aa178540ad608357
Delfinaortega1 <delfinaortega130@gmail.com>
2026-06-16 13:58:46 -0300

    fix script limpio

--- script.js
+++ script.js
@@ -158,17 +158,9 @@ if (!MODO_PROYECTOR) {
   });
 }
 
+// ——— PROYECCIÓN ———
 const VIDEOS = [
-  'aurora1.mov',
-  'aurora2.mov',
-  'aurora3.mov',
-  'aurora4.mov',
-  'aurora5.mov',
-  'aurora6.mov',
-  'aurora7.mov',
-  'aurora8.mov',
-  'aurora9.mov',
-  'aurora10.mov'
+  'aurora1.mp4'
 ];
 function abrirProyeccion() {
   const overlay = document.getElementById('proyeccion-overlay');
